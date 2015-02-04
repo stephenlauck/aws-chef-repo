788d0de561d61b0b68adc6bb33efe6c5f76ff401
Stephen Lauck <stephen.lauck@gmail.com>
2015-02-04 11:31:33 -0500

    remove validation info from knife.rb

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -5,8 +5,6 @@ log_level                :info
 log_location             STDOUT
 node_name                "flock"
 client_key               "#{current_dir}/flock.pem"
-validation_client_name   "example-validator"
-validation_key           "#{current_dir}/example-validator.pem"
 chef_server_url          'http://127.0.0.1:8889'
 cache_type               'BasicFile'
 cache_options( :path => "#{ENV['HOME']}/.chef/checksums" )
